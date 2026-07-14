3450bf547eda6c9f758bb32f33457b3c48a5c7ac
abcdavk <xloot.mcpedl@gmail.com>
2026-07-14 12:58:29 +0700

    update: add hero-section content and search bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,25 @@ export default function Home() {
     <main className="flex flex-col flex-1 font-sans">
       <section className="hero-section-container">
         <div className="hero-section-content">
-          <h1>Hweo section</h1>
-          <p>Lorem ipsum dolor sit amet.</p>
+          <h2 className="text-6xl font-bold">Temukan proyek dan lowongan</h2>
+          <h2 className="text-6xl font-bold text-[#344F1F]">yang cocok untuk keahlianmu!</h2>
+          
+          <div className="flex justify-center mt-8">
+            <form className="flex items-center rounded-xl bg-white shadow-lg">
+              <input
+                type="search"
+                placeholder="Keahlian atau perusahaan"
+                className="w-80 bg-transparent px-4 py-2 outline-none"
+              />
+
+              <button
+                type="button"
+                className="rounded-lg bg-primary px-6 py-2 transition hover:brightness-110"
+              >
+                Filter
+              </button>
+            </form>
+          </div>
         </div>
       </section>
       <section className="cta-container">
